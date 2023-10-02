d48461252d0555bb97ce34b7eb7efc17a4b0063d
kennystore2023 <146505519+kennystore2023@users.noreply.github.com>
2023-10-02 22:36:25 +0700

    update

--- src/main/java/com/example/projectswp/dbConnection/DBUtils.java
+++ src/main/java/com/example/projectswp/dbConnection/DBUtils.java
@@ -6,33 +6,31 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 public class DBUtils {
-    /*
     public static Connection makeConnection() throws Exception{
         Connection cn=null;
-        String uid=" ";
-        String pwd=" ";
+        String uid="db_a9fac3_kennybookstore_admin";
+        String pwd="kennybookstore1";
         String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String url="jdbc:sqlserver://SQL8003.site4now.net;databaseName= ;user="+uid+";password="+pwd;
+        String url="jdbc:sqlserver://SQL5111.site4now.net;databaseName=db_a9fac3_kennybookstore;user="+uid+";password="+pwd;
         Class.forName(driver);
         cn=DriverManager.getConnection(url);
         return cn;
     }
-    */
 
-    public static Connection makeConnection(){
-        try {
-            String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
-            String url = "jdbc:sqlserver://localhost:1433;databasename=Kenny;";
-            Class.forName(driver);
-            Connection cn=DriverManager.getConnection(url, "sa", "12345");
-            return cn;
-        } catch(ClassNotFoundException ex) {
-            ex.printStackTrace();
-        } catch(SQLException ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
+//    public static Connection makeConnection(){
+//        try {
+//            String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+//            String url = "jdbc:sqlserver://localhost:1433;databasename=Kenny;";
+//            Class.forName(driver);
+//            Connection cn=DriverManager.getConnection(url, "sa", "12345");
+//            return cn;
+//        } catch(ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        } catch(SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//        return null;
+//    }
 
     public static String getCurrentDate(){
         DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy/MM/dd");
